a9f4c9f24fadb5cd395b26c98709bf9f73d3443a
youngnh <youngnh@84e43a8a-2e3d-0410-b01a-eb7fda750bab>
2008-05-18 19:32:30 +0000

    working colliding demo, but with only two balls

--- branches/grand-theft-branch/Steering/src/demo/CollisionDemo.java
+++ branches/grand-theft-branch/Steering/src/demo/CollisionDemo.java
@@ -5,6 +5,8 @@ import geom.CenteredEllipse;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Shape;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import physics.Physics;
@@ -15,6 +17,8 @@ import vector.Vector3D;
 
 public class CollisionDemo extends ScreensaverBase {
 
+	private int numberOfBalls = 2;
+	
 	private double mass = 1.0;
 	private double maxForce = 100.0;
 	private double maxSpeed = 100.0;
@@ -25,7 +29,7 @@ public class CollisionDemo extends ScreensaverBase {
 	
 	private Shape circle = new CenteredEllipse(0.0, 0.0, radius * 2, radius * 2);
 	
-	private Orb green, blue;
+	private List<Orb> balls;
 	
 	@Override
 	public void init() {
@@ -33,31 +37,33 @@ public class CollisionDemo extends ScreensaverBase {
 	
 		Random random = new Random();
 		
-		Vector3D initPos;
+		balls = new ArrayList<Orb>();
 		
-		double x = random.nextDouble() * (width - 2 * radius) + radius;
-		double y = random.nextDouble() * (height - 2 * radius) + radius;
+		Vector3D initPos, initVelo;
+		double x, y;
 		
-		initPos = new Vector3D(x, y, 0.0);
-		Vector3D initVelo = new Vector3D(1.0, 1.0, 0.0);
-		green = new Orb(mass, initPos, initVelo, radius, maxForce, maxSpeed);
-
-		x = random.nextDouble() * (width - 2 * radius) + radius;
-		y = random.nextDouble() * (height - 2 * radius) + radius;
-		
-		initPos = new Vector3D(x, y, 0.0);
-		initVelo = new Vector3D(-1.0, 1.0, 0.0);
-		blue = new Orb(mass, initPos, initVelo, radius, maxForce, maxSpeed);
+		for(int i = 0; i < numberOfBalls; i++) {
+			x = random.nextDouble() * (width - 2 * radius) + radius;
+			y = random.nextDouble() * (height - 2 * radius) + radius;
+			
+			initPos = new Vector3D(x, y, 0.0);
+			initVelo = new Vector3D(1.0, 1.0, 0.0);
+			Orb ball = new Orb(mass, initPos, initVelo, radius, maxForce, maxSpeed);
+			balls.add(ball);
+		}
 	}
 	
 	@Override
-	public void render(Graphics2D g2) {
+	public void render(Graphics2D g2) {		
+		// draw all balls
 		g2.setColor(Color.GREEN);
-		fillAt(g2, circle, green.position().x + xoffset, green.position().y + yoffset);
-		g2.setColor(Color.BLUE);
-		fillAt(g2, circle, blue.position().x + xoffset, blue.position().y + yoffset);
+		for(Orb ball : balls) {
+			fillAt(g2, circle, ball.position().x + xoffset, ball.position().y + yoffset);
+		}
 		
 		// check for collision with balls
+		Orb green = balls.get(0);
+		Orb blue = balls.get(1);
 		if(intersecting(green, blue)) {
 			Vector3D[] results = 
 				Physics.collide(green.mass(), green.position(), green.velocity(),
@@ -69,8 +75,10 @@ public class CollisionDemo extends ScreensaverBase {
 		collideWithWall(green);
 		collideWithWall(blue);
 		
-		green.move();
-		blue.move();
+		// update all ball's positions
+		for(Orb ball : balls) {
+			ball.move();
+		}
 	}
 
 	private void collideWithWall(Orb orb) {
